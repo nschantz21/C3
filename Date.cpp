d1a83b51ffc67fd6af2155bdf399810fd5839706
nschantz21 <nschantz21@gmail.com>
2020-04-04 23:28:33 +0000

    consolidated files. added error check to month setter

--- Date.cpp
+++ Date.cpp
@@ -1,8 +1,13 @@
-/* 
+/*
  * Date.cpp
  * Nicholas Schantz
  * 2020-04-07
+ * 
  */
+
+#include <iostream>
+using std::cout;
+using std::cerr;
 
 class Date
 {   
@@ -16,15 +21,23 @@ class Date
         Date(int month, int day, int year)
         {
             // set attributes using "this" pointer
-            this->month = month;
-            this->day = day;
-            this->year = year;
+            if ((month < 1) || (month > 12)) {
+                cerr << "month must be between 1 and 12, inclusive\n";
+            } else {
+                this->month = month;
+                this->day = day;
+                this->year = year;
+            }
         }
 
         // get and set functions
         void setMonth(int month)
         {
-            this->month = month;
+            if ((month < 1) || (month > 12)) {
+                cerr << "month must be between 1 and 12, inclusive\n";
+            } else {
+                this->month = month;
+            }
         }
         
         int getMonth()
@@ -55,6 +68,32 @@ class Date
         // display
         void display()
         {
-            std::cout << month << "/" << day << "/" << year <<"\n";
+            cout << month << "/" << day << "/" << year <<"\n";
         }
+};
+
+
+int main()
+{
+    // initializer
+    Date myBirthday(4, 21, 1992);
+
+    // display
+    myBirthday.display();
+
+    // setters
+    myBirthday.setMonth(10);
+    myBirthday.setDay(5);
+    myBirthday.setYear(1999);
+
+    // getters
+    cout << "My new birthday: " <<
+        myBirthday.getMonth() << "/" <<
+        myBirthday.getDay() << "/" <<
+        myBirthday.getYear() << "\n";
+
+    // force error
+    myBirthday.setMonth(15);
+
+    return 0;
 }
--- Date.h
+++
@@ -1,7 +0,0 @@
-# ifndef DATE_H
-# define DATE_H
-
-// do I have to declare the class in the header file?
-Date;
-
-# endif
